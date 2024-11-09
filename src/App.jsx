98ad12f878b2c057c13cccabfec0e9787d84eaae
Rifannvl <riffanafendi@gmail.com>
2024-11-09 23:13:24 +0700

    memberikan base layout pada semua page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import BaseLayout from "./layout/BaseLayout";
 
 export default function App() {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <BaseLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BaseLayout>
+    </BrowserRouter>
+  );
 }
