f15aa1ef7c798cc152993feaa2f23d07870963a4
Rifannvl <riffanafendi@gmail.com>
2024-11-11 22:46:59 +0700

    menghilangkan profile pada section home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import NewsAndAnnouncements from "../ui/BeritaPengumuman";
 import Faq from "../ui/Faq";
 import HeroSection from "../ui/HeroSection";
 import SchoolCalendar from "../ui/KalenderSekolah";
-import Profile from "../ui/ProfileSekolah";
 import AcademicProgram from "../ui/ProgramAkademik";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <Profile />
       <AcademicProgram />
       <SchoolCalendar />
       <NewsAndAnnouncements />
