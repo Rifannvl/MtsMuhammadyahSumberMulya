a03bdda2079a9032f95be7d22d7481dde157cc0b
Rifannvl <riffanafendi@gmail.com>
2024-11-11 22:47:36 +0700

    membuat route ke profile dan halaman 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BaseLayout from "./layout/BaseLayout";
+import Profile from "./ui/ProfileSekolah";
 
 export default function App() {
   return (
@@ -8,6 +9,9 @@ export default function App() {
       <BaseLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BaseLayout>
     </BrowserRouter>
