3dd57561ee171864510c709de95baaa9608a4919
Rifannvl <riffanafendi@gmail.com>
2024-11-13 22:47:52 +0700

    menampilkan hasa section home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,11 @@
 import NewsAndAnnouncements from "../ui/BeritaPengumuman";
 import Faq from "../ui/Faq";
 import HeroSection from "../ui/HeroSection";
-import SchoolCalendar from "../ui/KalenderSekolah";
-import AcademicProgram from "../ui/ProgramAkademik";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <AcademicProgram />
-      <SchoolCalendar />
-      <NewsAndAnnouncements />
       <Faq />
     </div>
   );
