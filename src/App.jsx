147f249e3454ac526055ccca11819e4216d0af28
Rifannvl <riffanafendi@gmail.com>
2024-11-20 20:33:19 +0700

    menambahkan route ke masing masing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BaseLayout from "./layout/BaseLayout";
 import Profile from "./ui/ProfileSekolah";
+import AcademicProgram from "./ui/ProgramAkademik";
+import SchoolCalendar from "./ui/KalenderSekolah";
+import BeritaPengumuman from "./ui/BeritaPengumuman";
 
 export default function App() {
   return (
     <BrowserRouter>
       <BaseLayout>
-        <Routes>
+        <Routes future={{ v7_startTransition: true }}>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/program" element={<AcademicProgram />} />
+          <Route path="/kalender" element={<SchoolCalendar />} />
+          <Route path="/pengumuman" element={<BeritaPengumuman />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BaseLayout>
